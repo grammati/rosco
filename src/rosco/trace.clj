567f1101378fd44c0cd8c7e0dbdc3bbd3a4b6099
Chris Perkins <cperkins@rallydev.com>
2014-05-23 10:07:15 -0600

    Make trace collection explicit via trace-root.

--- src/rosco/trace.clj
+++ src/rosco/trace.clj
@@ -1,11 +1,25 @@
 (ns rosco.trace
   (:require [clojure.edn :as edn]
-            [robert.hooke :refer [add-hook clear-hooks]]))
+            [robert.hooke :refer [add-hook remove-hook]]))
 
 
-(def ^:dynamic *trace-depth* 0)
+(def ^:dynamic *trace-depth*
+  "Thread-local stack-depth into traced functions"
+  0)
 
-(def trace-data (atom nil))
+(def ^:dynamic *trace-data*
+  "Thread-local containing either:
+    - an atom containing a sequence of trace-data maps, if a
+      trace-collection is in-progress, or
+    - nil, if no trace-collection is in progress"
+  nil)
+
+(def traces
+  "Atom containing collected traces."
+  (atom []))
+
+
+;;; Utilities and stuff
 
 (defn ->ns
   "DWIM to get a namespace. Takes a string, namespace, or symbol."
@@ -14,30 +28,33 @@
    (string? ns) (->ns (edn/read-string ns))
    :else        (the-ns ns)))
 
-(defn ns-vars [ns filter-fn]
-  (->> ns ->ns ns-interns vals (filter filter-fn)))
+(defn ns-vars
+  "Returns a sequence of the vars in a namespace."
+  ([ns]
+     (ns-vars ns (constantly true)))
+  ([ns filter-fn]
+     (->> ns ->ns ns-interns vals (filter filter-fn))))
 
-(defn find-namespaces [pattern]
+(defn find-namespaces
+  "Returns a sequence of loaded namespaces whose names match the given
+  regular expression."
+  [pattern]
   (filter #(re-matches pattern (-> % .name name)) (all-ns)))
 
-(defn traceable? [v]
+(defn traceable?
+  "This is the default predictae for determining whether to add
+  tracing to a var when calling `trace-namespace`."
+  [v]
   (and (var? v)
        (fn? @v)
        (not (:macro (meta v)))
        (not (:no-trace (meta v)))))
 
-(defn trace-start
-  "Starts a new trace collection."
-  []
-  (reset! trace-data []))
-
-(defn- trace-indent []
-  (apply str (repeat *trace-depth* "  ")))
 
 (defn trace-enter
   "Called when entering a traced function"
   [id v args]
-  (swap! trace-data conj
+  (swap! *trace-data* conj!
          {:id    id
           :var   v
           :args  args
@@ -48,7 +65,7 @@
 (defn trace-leave
   "Called when leaving a traced function"
   [id v ret]
-  (swap! trace-data conj
+  (swap! *trace-data* conj!
          {:id    id
           :var   v
           :ret   ref
@@ -56,17 +73,43 @@
           :depth *trace-depth*
           :t     (System/nanoTime)}))
 
-(defn wrap-tracing [v]
-  (fn [f & args]
-    (let [id (gensym "trace")]
-      (when (zero? *trace-depth*)
-        (trace-start))
-      (trace-enter id v args)
-      (let [ret (binding [*trace-depth* (inc *trace-depth*)]
-                  (apply f args))]
-        (trace-leave id v ret)
-        ret))))
 
+(defn- call-with-tracing [v f args]
+  (let [call-id  (gensym "call")]
+    (trace-enter call-id v args)
+    (let [ret (binding [*trace-depth* (inc *trace-depth*)]
+                (apply f args))]
+      (trace-leave call-id v ret)
+      ret)))
+
+(defn wrap-tracing
+  "Wrap a var such that a call to it will collect trace data if and
+  only if a trace is already in-progress."
+  [v]
+  (fn [f & args]
+    (if *trace-data*
+      (call-with-tracing v f args)
+      (apply f args))))
+
+(defn wrap-trace-root
+  "Wrap a var such that calling it begins a trace collection, if one
+  is not already in progress."
+  [v]
+  (fn [f & args]
+    (if (nil? *trace-data*)
+      
+      ;; Set up a new trace collection
+      (let [trace-id (gensym "trace")]
+        (binding [*trace-data* (atom (transient []))]
+          (try
+            (call-with-tracing v f args)
+            (finally
+              (swap! traces conj {:trace-id   trace-id
+                                  :trace-data (persistent! @*trace-data*)
+                                  :thread     (Thread/currentThread)})))))
+
+      ;; Else: a trace collection is already in progress
+      (call-with-tracing v f args))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Public API
@@ -74,10 +117,22 @@
 (defn trace-var
   "Inject tracing into the var."
   [v]
+  (println "Tracing" v)
   (add-hook v ::trace (wrap-tracing v)))
 
+(defn untrace-var
+  "Remove tracing from a var."
+  [v]
+  (println "Untracing" v)
+  (remove-hook v ::trace))
+
+(defn trace-root
+  "Wrap a var so that calling it begins a trace collection."
+  [v]
+  (add-hook v ::trace (wrap-trace-root v)))
+
 (defn trace-namespace
-  "Given a namespace object, add tracing to all vars that contain a function."
+  "Add tracing to all vars in a namspace."
   ([]
      (trace-namespace *ns*))
   ([ns]
@@ -85,6 +140,14 @@
   ([ns filter-fn]
      (doseq [v (ns-vars ns filter-fn)]
        (trace-var v))))
+
+(defn untrace-namespace
+  "Remove tracing from all vars in a namespace."
+  ([]
+     (untrace-namespace *ns*))
+  ([ns]
+     (doseq [v (ns-vars ns)]
+       (untrace-var v))))
 
 (defn trace-namespaces
   "Traces all loaded namespaces whose names match the given regular expression."
@@ -94,10 +157,17 @@
      (doseq [ns (find-namespaces ns-pattern)]
        (trace-namespace ns var-filter))))
 
+(defn untrace-namespaces
+  "Untrace namespaces whose names match the given pattern."
+  ([ns-pattern]
+     (doseq [ns (find-namespaces ns-pattern)]
+       (untrace-namespace ns))))
+
+
 (defn print-trace!
   "Prints out the last collected trace."
   ([]
-     (print-trace! @trace-data {}))
+     (print-trace! (:trace-data (last @traces)) {}))
   ([[d & data] start-times]
      (when d
        (println (apply str (repeat (:depth d) "  "))
@@ -108,3 +178,6 @@
        (recur data (if (= :enter (:type d))
                      (assoc start-times (:id d) (:t d))
                      start-times)))))
+
+(defn clear-traces! []
+  (reset! traces []))
