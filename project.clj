4d45c7378846bce4126d89e70d91b33194675bcd
Chris Perkins <chrisperkins99@gmail.com>
2017-06-02 15:43:37 -0600

    Update deps

--- project.clj
+++ project.clj
@@ -5,17 +5,16 @@
   :url "http://github.com/grammati/rosco"
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [robert/hooke "1.3.0"]
-                 ]
+                 [robert/hooke "1.3.0"]]
 
   :plugins [[lein-figwheel "0.5.2"]
             [lein-cljsbuild "1.1.3" :exclusions [[org.clojure/clojure]]]]
 
-  :profiles {:dev   {:dependencies [[ring "1.3.1"]
+  :profiles {:dev   {:dependencies [[ring "1.6.1"]
                                     [ring-mock "0.1.5"]
-                                    [org.clojure/clojurescript "1.7.228"]
-                                    [reagent "0.5.1"]
-                                    [figwheel-sidecar "0.5.2"]
+                                    [org.clojure/clojurescript "1.9.562"]
+                                    [reagent "0.6.2"]
+                                    [figwheel-sidecar "0.5.10"]
                                     [com.cemerick/piggieback "0.2.1"]]}
 
              :debug {:jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5007"]}}
